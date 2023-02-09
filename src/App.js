32f32aa332778c5aaa01797809c40c9816350195
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2023-02-09 10:38:35 +0200

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Example from "./navbar/navbar"
+import Login from "./login/login"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Example/>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
